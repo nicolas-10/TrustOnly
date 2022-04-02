7ed3dc8cda4857d40851dfc9abcb3d2876cc4939
Nicolas <alawenicolas@gmail.com>
2022-04-02 04:21:56 +0000

    front end project with Vuejs and Semantic-ui

---
+++ Js/app.js
@@ -0,0 +1,74 @@
+
+new Vue({
+    el:'#app',
+    data:{
+        products: [
+            {
+                _id: "1",
+                img: "https://source.unsplash.com/301x301/?random",
+                price: 15000,
+                name: "Random article",
+                likes: 100,
+                comments: [
+                    "Super cool lol", 
+                    "Article en très bon état à la réception"
+                ],
+            },
+            {
+                _id: "2",
+                img: "https://source.unsplash.com/301x301/?random",
+                price: 15000,
+                name: "Random article",
+                likes: 3,
+                comments: [
+                    "ah merde c'est pourri", 
+                    "Article en très bon état à la réception"
+                ],
+            },
+            {
+                _id: "3",
+                img: "https://source.unsplash.com/301x301/?random",
+                price: 15000,
+                name: "Random article",
+                likes: 12,
+                comments: [
+                    "Super cool lol", 
+                    "Article en très bon état à la réception"
+                ],
+            },
+            {
+                _id: "4",
+                img: "https://source.unsplash.com/301x301/?random",
+                price: 15000,
+                name: "Random article",
+                likes: 10,
+                comments: [
+                    "Ouii, ouiiiiiii", 
+                    "Article en très bon état à la réception"
+                ],
+            },
+            {
+                _id: "5",
+                img: "https://source.unsplash.com/301x301/?random",
+                price: 15000,
+                name: "Random article",
+                likes: 10,
+                comments: [
+                    "Ouii, ouiiiiiii", 
+                    "Article en très bon état à la réception"
+                ],
+            },
+            {
+                _id: "5",
+                img: "https://source.unsplash.com/301x301/?random",
+                price: 15000,
+                name: "Random article",
+                likes: 10,
+                comments: [
+                    "Ouii, ouiiiiiii", 
+                    "Article en très bon état à la réception"
+                ],
+            },
+        ] 
+    }
+})
